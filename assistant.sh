8bba670fcc2b1bbcb3e03618024b07bfb4f8ec03
yx <yangx21@mails.tsinghua.edu.cn>
2023-02-25 13:53:13 +0800

    add ROS and isaacgm

--- assistant.sh
+++ assistant.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 sudo apt install -y xterm zenity curl
-Codename=$(lsb_release -sc)
-echo "检测到您的Ubuntu系统版本为：$Codename"
+codename=$(lsb_release -sc)
+echo "检测到您的Ubuntu系统版本为：$codename"
 username=$(whoami)
 
 resize -s 45 90
 SELECT=$(whiptail --title "Ubuntu助手" --checklist \
 "选择要安装的软件或电脑配置（可多选，空格键选择，Tab键跳转)" 45 90 30 \
 "换源&Clean" "  删除不需要的软件" OFF \
+"Julia" "安装Julia" OFF \
+"kazam" "录屏软件" OFF \
+"Vim配置" "  Vim自定义配置" OFF \
 "键盘配置" "    对调Esc和Caps" OFF \
 "Git" "版本管理软件" OFF \
 "Fish" "更智能的终端" OFF \
 "搜狗拼音输入法" "       Linux版搜狗拼音输入法" OFF \
+"重命名folder" "documents, downloads etc." OFF \
 "VSCode" "代码编辑器，功能强大、易用" OFF \
+"Miniconda3" "Python虚拟环境管理器" OFF \
+"Isaacgym" "Isaacgym" OFF \
+"sysmonitor&stacer&backintime" "安装监控软件sysmonitor, stacer and backintime" OFF \
 "代理软件" "    Clash for Windows" OFF \
 "Ubuntu美化" "  Ubuntu美化" OFF \
 "Google Chrome" "谷歌浏览器" OFF \
 "Zotero" "文献管理软件" OFF \
+"ROS" "安装ROS" OFF \
 "ROS2" "安装ROS2" OFF \
-"Miniconda3" "Python虚拟环境管理器" OFF \
 "NVIDIA显卡驱动" "    安装此项后安装CUDA时就不需选择Driver了" OFF \
-"sysmonitor&stacer&backintime" "安装监控软件sysmonitor, stacer and backintime" OFF \
 "VirtualBox" "虚拟机软件" OFF \
-"Julia" "安装Julia" OFF \
-"kazam" "录屏软件" OFF \
-"Vim配置" "  Vim自定义配置" OFF \
 3>&1 1>&2 2>&3
 )
 
@@ -115,9 +118,9 @@ function config_clean {
     mkdir -p ~/.config/flameshot
     cp ./dotfile/flameshot.ini ~/.config/flameshot/
     # lolcate配置
-    sudo mv ./dotfile/lolcate/lolcate /usr/bin/
-    lolcate --create
-    sed -i "s/caslx/$username/g" ./dotfile/lolcate/default/config.toml
+    sudo cp ./dotfile/lolcate/lolcate /usr/bin/
+    /usr/bin/lolcate --create
+    sed -i "s/caslx/${username}/g" ./dotfile/lolcate/default/config.toml
     cp -rf ./dotfile/lolcate ~/.config
     
     # pip换清华源
@@ -185,13 +188,88 @@ function config_sogou {
     config_success
 }
 
+function config_folder {
+    echo -e "${BGreen}将要重命名folder${Color_Off}" && sleep 1s
+    cp ./dotfile/user-dirs.dirs ~/.config/
+    mv ~/下载 ~/downloads
+    mv ~/文档 ~/documents
+    mv ~/桌面 ~/desktop
+    mv ~/音乐 ~/music
+    mv ~/视频 ~/videos
+    mv ~/图片 ~/pictures
+    mv ~/公共 ~/public
+    mv ~/模板 ~/templates
+    config_success
+}
+
 function config_vscode {
     echo -e "${BGreen}将要安装VSCode${Color_Off}" && sleep 1s
     cd
-    wget -O code.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 
+    wget -O code.deb --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
     sudo dpkg -i code.deb
     rm code.deb
     success
+}
+
+function config_miniconda {
+    echo -e "${UBlue}将要安装miniconda${Color_Off}" && sleep 1s
+    cd
+    wget -O miniconda.sh https://mirror.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh
+    chmod +x miniconda.sh
+    ./miniconda.sh
+    rm miniconda.sh
+    # 配置环境变量
+    conda init bash
+    conda init fish
+    conda config --set auto_activate_base false
+    # 换源
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+    echo_out "conda换为清华源"
+    success
+}
+
+function config_isaacgym {
+	echo -e "${BGreen}将要安装isaac gym${Color_Off}" && sleep 1s
+    cd ~ && wget -O IsaacGym_Preview_4_Package.tar.gz https://cloud.tsinghua.edu.cn/f/36660a82fd344874be10/?dl=1
+    tar -xzvf IsaacGym_Preview_4_Package.tar.gz
+    conda create -n legged -y python=3.8
+    conda activate legged
+	cd IsaacGym_Preview_4_Package/python && pip install -e .
+	cd examples && python 1080_balls_of_solitude.py
+    echo_out "isaac gym 安装完成"
+}
+
+function config_julia {
+    echo -e "${BGreen}将要安装julia${Color_Off}" && sleep 1s
+    cd
+    wget -O julia.tar.gz https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.3-linux-x86_64.tar.gz
+    mkdir -p ~/softwares/julia
+    tar -zxvf julia.tar.gz -C ~/softwares/julia --strip-components 1
+    # 配置环境变量
+    echo "# install julia" >> ~/.bashrc
+    echo "export PATH=\$PATH:~/softwares/julia/bin" >> ~/.bashrc
+    echo "export JULIA_DEPOT_PATH=~/softwares/julia_package" >> ~/.bashrc
+    echo "# julia" >> ~/.config/fish/config.fish
+    echo "set -x fish_user_paths ~/softwares/julia/bin \$fish_user_paths" >> ~/.config/fish/config.fish
+    echo "set -x JULIA_DEPOT_PATH ~/softwares/julia_package" >> ~/.config/fish/config.fish
+    rm ~/julia.tar.gz
+    success
+}
+
+function config_kazam {
+    echo -e "${BGreen}将要安装kazam${Color_Off}" && sleep 1s
+    sudo apt -y install kazam
+    sudo cp ./kazam/*.py /usr/lib/python3/dist-packages/kazam/backend
+    success
+}
+
+function config_vim {
+    echo -e "${BGreen}将要配置vim${Color_Off}" && sleep 1s
+    cd
+    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
+    config_success
 }
 
 function config_proxy {
@@ -224,6 +302,23 @@ function config_proxy {
     config_success
 }
 
+function config_sysmonitor {
+    echo -e "${BGreen}将要安装sysmonitor, stacer, backintime...${Color_Off}"
+    # 安装sysmonitor
+    sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor
+    sudo apt update
+    sudo apt -y install indicator-sysmonitor stacer backintime-qt4
+    # copy displayIP.sh
+    mkdir ~/softwares
+    cp ./dotfile/displayIP.sh ~/softwares/
+    chmod +x ~/softwares/displayIP.sh 
+    cp .dotfile/indicator-sysmonitor.json ~/
+    # stacer开机自启动
+    echo -e "[Desktop Entry]\\nName=Stacer\\nComment=Linux System Optimizer and Monitoring\\nExec=env QT_SCALE_FACTOR=0.75 QT_AUTO_SCREEN_SCALE_FACTOR=1 stacer --hide\\nType=Application\\nTerminal=false\\nHidden=false" > ~/.config/autostart/stacer.desktop
+    echo -e "${BGreen}添加/home/${username}/softwares/displayIP.sh到indicator-sysmonitor${Color_Off}"
+    success
+}
+
 #################################################################################################################
 
 function config_beautify {
@@ -239,8 +334,9 @@ function config_beautify {
     unzip -o backgrounds.zip
     unzip -o gnome-shell.zip
     mv ~/backgrounds ~/.local/share/
-    mv ~/gnome-shell ~/.local/share/
+    mv ~/gnome-shell/* ~/.local/share/
     rm backgrounds.zip gnome-shell.zip
+    rm -rf gnome-shell
     # workspace 快捷键设置
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Primary>Left']"
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Primary>Right']"
@@ -255,6 +351,7 @@ function config_beautify {
     gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
     gsettings set org.gnome.desktop.interface enable-hot-corners 'true'
     gsettings set org.gnome.desktop.interface font-hinting 'slight'
+    # 
     
     config_success
 }
@@ -272,7 +369,7 @@ function config_zotero {
     echo -e "${BGreen}将要安装zotero${Color_Off}" && sleep 1s
     cd 
     mkdir -p ~/softwares/zotero && cd ~/softwares
-    wget -O zotero.tar.gz https://www.zotero.org/download/client/dl?channel=release&platform=linux-x86_64&version=6.0.18
+    wget -O zotero.tar.bz2 --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" https://www.zotero.org/download/client/dl?channel=release&platform=linux-x86_64&version=6.0.18
     wget https://github.com/jlegewie/zotfile/releases/download/v5.1.2/zotfile-5.1.2-fx.xpi
     tar -jxvf zotero.tar.bz2 -C ~/softwares/zotero --strip-components 1
     # update the .desktop file for that location
@@ -284,17 +381,39 @@ function config_zotero {
     success
 }
 
+function config_ROS {
+    echo -e "${BGreen}将要安装ROS${Color_Off}"
+    ROS_DISTRO=noetic
+    if test "$codename" = "focal"; then
+        ROS_DISTRO=noetic
+        sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+        sudo apt install curl # if you haven't already installed curl
+        curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+        sudo apt update
+        sudo apt install ros-$ROS_DISTRO-desktop-full
+        sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+        sudo apt install python3-rosdep
+        sudo rosdep init
+        rosdep update
+    else
+        echo -e "${BRed}仅支持ubuntu20，安装失败...${Color_Off}"
+        return -1
+    fi
+}
+
 function config_ROS2 {
     echo -e "${BGreen}将要安装ROS2${Color_Off}"
     ROS_DISTRO=humble
-    if test "$Codename" = "foxy"; then
+    if test "$codename" = "focal"; then
+        ROS_DISTRO=foxy
+    else if test "$codename" = "jammy"; then
         ROS_DISTRO=humble
-        sudo apt -y install python3-argcomplete
     else
         echo -e "${BRed}仅支持ubuntu20和22，安装失败...${Color_Off}"
         return -1
     fi
     # ensure that the Ubuntu Universe repository is enabled.
+    sudo apt -y install python3-argcomplete
     sudo apt -y install software-properties-common
     sudo add-apt-repository universe
     sudo apt update && sudo apt install curl
@@ -308,28 +427,10 @@ function config_ROS2 {
     echo source /opt/ros/$ROS_DISTRO/setup.bash >> ~/.bashrc
     # 写入fish config环境变量
     echo "#ROS2 $ROS_DISTRO" >> ~/.config/fish/config.fish
-    echo source /opt/ros/$ROS_DISTRO/setup.bash >> ~/.config/fish/config.fish
+    echo bass source /opt/ros/$ROS_DISTRO/setup.bash >> ~/.config/fish/config.fish
     # bass in fish
     omf install bass
     config_success
-}
-
-function config_miniconda {
-    echo -e "${UBlue}将要安装miniconda${Color_Off}" && sleep 1s
-    cd
-    wget -O miniconda.sh https://mirror.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh
-    chmod +x miniconda.sh
-    ./miniconda.sh
-    rm miniconda.sh
-    # 配置环境变量
-    conda init bash
-    conda init fish
-    conda config --set auto_activate_base false
-    # 换源
-    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
-    echo_out "conda换为清华源"
-    success
 }
 
 function config_nvidia {
@@ -346,52 +447,10 @@ function config_nvidia {
     success
 }
 
-function config_sysmonitor {
-    echo -e "${BGreen}将要安装sysmonitor, stacer, backintime...${Color_Off}"
-    # 安装sysmonitor
-    sudo add-apt-repository ppa:fossfreedom/indicator-sysmonitor
-    sudo apt update
-    sudo apt -y install indicator-sysmonitor stacer backintime-qt4
-    # stacer开机自启动
-    echo -e"[Desktop Entry]\\nName=Stacer\\nComment=Linux System Optimizer and Monitoring\\nExec=env QT_SCALE_FACTOR=0.75 QT_AUTO_SCREEN_SCALE_FACTOR=1 stacer --hide\\nType=Application\\nTerminal=false\\nHidden=false" > ~/.config/autostart/stacer.desktop
-    success
-}
-
 function config_virtualbox {
     echo -e "${BGreen}将要安装virtualbox${Color_Off}" && sleep 1s
     sudo apt install -y virtualbox
     success
-}
-
-function config_julia {
-    echo -e "${BGreen}将要安装julia${Color_Off}" && sleep 1s
-    cd
-    wget -O julia.tar.gz https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.3-linux-x86_64.tar.gz
-    mkdir -p ~/softwares/julia
-    tar -zxvf julia.tar.gz -C ~/softwares/julia --strip-components 1
-    # 配置环境变量
-    echo "# install julia" >> ~/.bashrc
-    echo "export PATH=$PATH:~/softwares/julia/bin" >> ~/.bashrc
-    echo "export JULIA_DEPOT_PATH=~/softwares/julia_package" >> ~/.bashrc
-    echo "set -x fish_user_paths ~/softwares/julia/bin $fish_user_paths" >> ~/.bashrc
-    echo "set -x JULIA_DEPOT_PATH ~/softwares/julia_package" >> ~/.bashrc
-    rm ~/julia.tar.gz
-    success
-}
-
-function config_kazam {
-    echo -e "${BGreen}将要安装kazam${Color_Off}" && sleep 1s
-    sudo apt -y install kazam
-    sudo cp ./kazam/*.py /usr/lib/python3/dist-packages/kazam/backend
-    success
-}
-
-function config_vim {
-    echo -e "${BGreen}将要配置vim${Color_Off}" && sleep 1s
-    cd
-    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
-    config_success
 }
 
 #################################################################################################################
@@ -410,6 +469,7 @@ if [ $existstatus = 0 ]; then
     echo $SELECT | grep "Ubuntu美化" && config_beautify
     echo $SELECT | grep "Google Chrome" && config_chrome
     echo $SELECT | grep "Zotero" && config_zotero
+    echo $SELECT | grep "ROS" && config_ROS
     echo $SELECT | grep "ROS2" && config_ROS2
     echo $SELECT | grep "Miniconda3" && config_miniconda
     echo $SELECT | grep "Nvidia显卡驱动" && config_nvidia
@@ -418,6 +478,8 @@ if [ $existstatus = 0 ]; then
     echo $SELECT | grep "Julia" && config_julia
     echo $SELECT | grep "kazam" && config_kazam
     echo $SELECT | grep "Vim配置" && config_vim
+    echo $SELECT | grep "重命名folder" && config_folder
+    echo $SELECT | grep "Isaacgym" && config_isaacgym
     
     if [ $Flag_Doc -eq 1 ]
     then 
